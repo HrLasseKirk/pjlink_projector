1103354582310d3df38421f2978929b5c107cc4f
HrLasseKirk <hrlassekirk@gmail.com>
2026-01-29 13:16:44 +0100

    Fix config flow handler

--- custom_components/pjlink_projector/config_flow.py
+++ custom_components/pjlink_projector/config_flow.py
@@ -1,3 +1,44 @@
+from __future__ import annotations
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_PASSWORD
+
+DOMAIN = "pjlink_projector"
+DEFAULT_PORT = 4352
+
+
+class PJLinkProjectorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for PJLink Projector."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            await self.async_set_unique_id(user_input[CONF_HOST])
+            self._abort_if_unique_id_configured()
+
+            return self.async_create_entry(
+                title=user_input[CONF_HOST],
+                data=user_input,
+            )
+
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_HOST): str,
+                vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
+                vol.Optional(CONF_PASSWORD): str,
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+            errors=errors,
+        )
 {\rtf1\ansi\ansicpg1252\cocoartf2867
 \cocoatextscaling0\cocoaplatform0{\fonttbl}
 {\colortbl;\red255\green255\blue255;}
